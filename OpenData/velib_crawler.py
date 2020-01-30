b9c2f6da5f203cdbf0dcac18e4fbef8114038e74
Barthélémy Launet <blaunet@student.ethz.ch>
2020-01-31 00:59:54 +0100

    cleaning

--- OpenData/velib_crawler.py
+++ OpenData/velib_crawler.py
@@ -3,30 +3,6 @@ import pandas as pd
 import datetime
 import os
 
-
-# def crawl_velib(save=False, path=None):
-#     dataset='velib-disponibilite-en-temps-reel'
-#
-#     ##first get total number of rows
-#     params={'rows':1}
-#     r = get(dataset, **params)
-#
-#     ##then query all rows
-#     params={'rows':r.json()['nhits']}
-#     query_datetime=datetime.datetime.now()
-#     r = get(dataset, **params)
-#
-#     data = [record['fields'] for record in r.json()['records']]
-#     df = pd.DataFrame(data)
-#     df['query_datetime'] = query_datetime
-#
-#     if save:
-#         if not path:
-#             dir_path = os.path.dirname(os.path.realpath(__file__))
-#             path = dir_path+'/data/velib_dispo_{}.csv'.format(query_datetime.strftime("%Y_%m_%d_%H_%M_%S"))
-#         df.to_csv(path)
-#     return df
-
 velib_crawler = OpenDataCrawler('velib-disponibilite-en-temps-reel')
 ##first get total number of rows
 params={'rows':1}
@@ -43,4 +19,4 @@ df['query_datetime'] = query_datetime
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 path = dir_path+'/data/velib_dispo_{}.csv'.format(query_datetime.strftime("%Y_%m_%d_%H_%M_%S"))
-df.to_csv(path)
+df.to_csv(path, index=False)
