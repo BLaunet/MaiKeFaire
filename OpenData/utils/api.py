11648477736600d739e577044ec9c4f0897e6535
Barthélémy Launet <blaunet@student.ethz.ch>
2020-01-31 00:47:45 +0100

    saving results

--- OpenData/utils/api.py
+++ OpenData/utils/api.py
@@ -2,7 +2,13 @@ import requests
 
 BASE_URL = 'https://opendata.paris.fr/api/records/1.0/search/'
 
-def get(dataset, **params):
-    p='&'.join(['{}={}'.format(k,v) for k,v in params.items()])
-    url = BASE_URL+'?dataset='+dataset+'&'+p
-    return requests.get(url)
+
+
+class OpenDataCrawler:
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    def get(self, **params):
+        p='&'.join(['{}={}'.format(k,v) for k,v in params.items()])
+        url = BASE_URL+'?dataset='+self.dataset+'&'+p
+        return requests.get(url)
--- OpenData/velib_crawler.py
+++ OpenData/velib_crawler.py
@@ -1,20 +1,46 @@
-from utils import get
+from utils.api import OpenDataCrawler
 import pandas as pd
 import datetime
+import os
 
-def crawl_velib():
-    dataset='velib-disponibilite-en-temps-reel'
 
-    ##first get total number of rows
-    params={'rows':1}
-    r = get(dataset, **params)
+# def crawl_velib(save=False, path=None):
+#     dataset='velib-disponibilite-en-temps-reel'
+#
+#     ##first get total number of rows
+#     params={'rows':1}
+#     r = get(dataset, **params)
+#
+#     ##then query all rows
+#     params={'rows':r.json()['nhits']}
+#     query_datetime=datetime.datetime.now()
+#     r = get(dataset, **params)
+#
+#     data = [record['fields'] for record in r.json()['records']]
+#     df = pd.DataFrame(data)
+#     df['query_datetime'] = query_datetime
+#
+#     if save:
+#         if not path:
+#             dir_path = os.path.dirname(os.path.realpath(__file__))
+#             path = dir_path+'/data/velib_dispo_{}.csv'.format(query_datetime.strftime("%Y_%m_%d_%H_%M_%S"))
+#         df.to_csv(path)
+#     return df
 
-    ##then query all rows
-    params={'rows':r.json()['nhits']}
-    query_datetime=datetime.datetime.now()
-    r = get(dataset, **params)
+velib_crawler = OpenDataCrawler('velib-disponibilite-en-temps-reel')
+##first get total number of rows
+params={'rows':1}
+r = velib_crawler.get(**params)
 
-    data = [record['fields'] for record in r.json()['records']]
-    df = pd.DataFrame(data)
-    df['query_datetime'] = query_datetime
-    return df
+##then query all rows
+params={'rows':r.json()['nhits']}
+query_datetime=datetime.datetime.now()
+r = velib_crawler.get(**params)
+
+data = [record['fields'] for record in r.json()['records']]
+df = pd.DataFrame(data)
+df['query_datetime'] = query_datetime
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+path = dir_path+'/data/velib_dispo_{}.csv'.format(query_datetime.strftime("%Y_%m_%d_%H_%M_%S"))
+df.to_csv(path)
